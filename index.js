11fdb02f28112da243fd384783e1771324d1513c
Patrick Dutra <patrickdutra100@hotmail.com>
2022-09-06 16:41:45 -0300

    buttonRestaurar

--- index.js
+++ index.js
@@ -15,17 +15,37 @@ function apagaLampada() {
 }
 
 function quebraLampada() {
-    const imagem = document.getElementById("lampada");
-    imagem.src = "/images/lampada_quebrada.jpg";
-    quebrada = true;
-
-    const button =
-        '<button onclick="restaurarLampada()">Restaurar Lâmpada</button>';
+    if (!quebrada) {
+        
+        const imagem = document.getElementById("lampada");
+        imagem.src = "/images/lampada_quebrada.jpg";
+        quebrada = true;
+        
+        // const button =
+        //     '<button onclick="restaurarLampada()">Restaurar Lâmpada</button>';
+        // const divPrincipal = document.getElementById('principal');
+        // divPrincipal.innerHTML = divPrincipal.innerHTML + button;
+        
+        //Modo 2 usando o createElement
+        
+        const button = document.createElement('button')
+        button.innerText = "Restaurar Lâmpada!"
+        button.onclick = restaurarLampada
+        button.id = 'restaurar'
+        
+        const divPrincipal = document.getElementById('principal')
+        divPrincipal.appendChild(button)
+    }
 }
 
-function restaurarLampada() {
+    function restaurarLampada() {
     // const imagem = document.getElementById("lampada");
     // imagem.src = "/images/lampada_apagada.jpg";
     quebrada = false;
     apagaLampada();
+
+    const divPrincipal = document.getElementById('principal')
+    const button = document.getElementById('restaurar')
+
+    divPrincipal.removeChild(button)
 }
